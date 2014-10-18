b37c2eadee57ad18439a77c0955d15550fe22aa4
greg <gregsohl@gmail.com>
2014-10-18 16:26:06 -0500

    New MVC 5 Projects, as created by Visual Studio

---
+++ MyAvatar/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyAvatar.Startup))]
+namespace MyAvatar
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
